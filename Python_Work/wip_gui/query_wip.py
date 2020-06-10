56bfb891e801babeba70c11a6c1d678f79af9252
ypguest <ypguest@163.com>
2020-06-10 16:39:10 +0800

    update

--- Python_Work/wip_gui/query_wip.py
+++ Python_Work/wip_gui/query_wip.py
@@ -115,13 +115,18 @@ def lot_wafer():
 
     # todo 根据Mother lot id对数据进行遍历，并根据遍历过程中Wafer No为1的数据，读出Wafer No, 并将Lot Wafer信息进行写入psmc_lot_wafer数据库中
     for mlot in list_mlot:
-        dictlot = {}
         data = df[df.loc[:, 'MLot_ID'] == mlot].copy()  # 按lot生成data数据,包含（MLot_ID，Lot_ID，Current_Chip_Name，Current_Time，Wafer信息）
         data.sort_values(by='Current_Time', axis=0, ascending=False, inplace=True, na_position='first')
         for index, row in data.iteritems():
             if row.name in ['#01', '#02', '#03', '#04', '#05', '#06', '#07', '#08', '#09', '#10',
                             '#11', '#12', '#13', '#14', '#15', '#16', '#17', '#18', '#19', '#20', '#21', '#22', '#23', '#24', '#25']:
-                for i in range(len(row.values)):
+                # todo 按Wafer No对TJS Lot id进行写入
+                dictlot = {}  # 创建字典，用于装载BE信息
+                dictlot['Current_Chip_Name'] = str(data['Current_Chip_Name'].values[-1])  # 由于Current_Chip_Name有好几个，选择最后一个
+                dictlot['Mlot_ID'] = mlot
+                dictlot['Lot_Wafer_No'] = mlot + row.name
+                dictlot['Wafer_No'] = row.name
+                for i in range(len(row.values)):   # 如果某一片Wafer在不同的lot_id中重复，需要循环重复的次数；
                     if row.values[i] == '1':
                         # todo 连接tjs_wo_tracking_table数据库，按mlot，Wafer No获取每片Wafer的TJS lot id
                         connection = pymysql.connect(**sql_config)
@@ -133,30 +138,23 @@ def lot_wafer():
                             columnDes = cursor.description
                             columnNames = [columnDes[i][0] for i in range(len(columnDes))]
                             connection.close()
-                            
                             #  todo 获取wo中BE_Lot_ID,Picking_Sorts，Wafer No的对应关系
                             for result in sql_results:
                                 if result[4] == '1':   # 选取Wafer No为1的bin
                                     for binNo in result[3].split(','):
-                                        dictlot['Current_Chip_Name'] = str(data['Current_Chip_Name'].values[-1])   # 由于Current_Chip_Name有好几个，选择最后一个
-                                        print(dictlot)
-                                    
-
-
-                                        # print(str(data['Current_Chip_Name'].values[-1]), mlot, mlot, row.name, row.name)
-                        # connection = pymysql.connect(**sql_config)
-                        # with connection.cursor() as cursor:
-                        #     # noinspection PyBroadException
-                        #     try:
-                        #         cursor.execute('USE testdb;')
-                        #         cursor.execute("INSERT INTO psmc_lot_wafer(Current_Chip_Name, Mlot_ID, Lot_Wafer_No, Wafer_No)"
-                        #                        "VALUES ('%s','%s','%s%s','%s')" % (str(data['Current_Chip_Name'].values[-1]), mlot, mlot, row.name, row.name))
-                        #         connection.commit()
-                        #         cursor.close()
-                        #         connection.close()
-                        #     except Exception:
-                        #         continue
-
+                                        print(dictlot, binNo)
+                                        if binNo.strip() == '01~11':
+                                            dictlot['BE_Lot_ID(Bin01~11)'] = result[1]
+                                        elif binNo.strip() == '16~21':
+                                            dictlot['BE_Lot_ID(Bin16~21)'] = result[1]
+                                        else:
+                                            dictlot['BE_Lot_ID(Other)'] = result[1]
+                                        print(dictlot, binNo)
+                        continue   # 只要有一个成立，则跳出for循环
+                dflot = pd.DataFrame.from_dict([dictlot], orient='columns')   # 将生成的dictlot信息转化为DataFrame
+                dflot = dflot.reindex(columns=['Current_Chip_Name', 'Mlot_ID', 'Lot_Wafer_No', 'Wafer_No', 'BE_Lot_ID(Bin01~11)', 'BE_Lot_ID(Bin16~21)', 'BE_Lot_ID(Other)'], fill_value=np.nan)
+                myconnect = create_engine('mysql+mysqldb://root:yp*963.@localhost:3306/testdb?charset=utf8')
+                pd.io.sql.to_sql(dflot, 'psmc_lot_wafer', con=myconnect, schema='testdb', if_exists='append', index=False)
 
 
 if __name__ == "__main__":
